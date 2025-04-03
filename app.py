ea3a3d9c891251741a42ecc00c661a7e4db6fb6f
DonatasNoreika <donoras@gmail.com>
2025-04-03 16:03:32 +0300

    Feature 2 AB

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Feature 1 AB")
+print("Feature 2 AB")
+print("Feature 3 B")
+
+for x in range(10):
+    print("Hi")
+
+print("Feature more 1 AB")
+print("Feature more 2 AB")
+print("Feature more 3 B")

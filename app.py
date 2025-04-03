9aa77ad8853420bd598225f9d89a5d4726a6279a
DonatasNoreika <donoras@gmail.com>
2025-04-03 16:04:56 +0300

    Feature 4 A

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 print("Feature 1 AB")
 print("Feature 2 AB")
 print("Feature 3 A")
+print("Feature 4 A")
 
 for x in range(10):
     print("Hi")
@@ -8,3 +9,4 @@ for x in range(10):
 print("Feature more 1 AB")
 print("Feature more 2 AB")
 print("Feature more 3 A")
+print("Feature more 4 A")
